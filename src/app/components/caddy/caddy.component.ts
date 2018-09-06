25bbc0ce22c536e0b6fb93fc94b14e98cb4526f9
Frédérick PAREJA <frederick.pareja.pf1@gmail.com>
2018-09-06 17:52:22 +0200

    update css

--- src/app/components/caddy/caddy.component.ts
+++ src/app/components/caddy/caddy.component.ts
@@ -1,4 +1,6 @@
+import { OrdersContent } from './../../models/orders-content';
 import { Component, OnInit } from '@angular/core';
+import { Orders } from '../../models/orders';
 
 @Component({
   selector: 'app-caddy',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./caddy.component.css']
 })
 export class CaddyComponent implements OnInit {
+  private orderInProgress: Orders; // attribut to manage a user order in progress
+  private caddyInProgress: OrdersContent[]; // attribut to manage user purchases
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**********************
+  **********************
+  * GETTERS AND SETTERS
+  **********************
+  *********************/
+
+  public getOrderInProgress(): Orders {
+    return this.orderInProgress;
+  }
+
+  public getCaddyInProgress(): OrdersContent[] {
+    return this.caddyInProgress;
   }
 
 }
